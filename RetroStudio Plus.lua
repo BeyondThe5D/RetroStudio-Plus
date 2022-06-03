88fbadbdaedbc8f336526fc95edc25c3e39a8490
BeyondThe5D <92761598+BeyondThe5D@users.noreply.github.com>
2022-06-03 03:57:42 +0100

    Update RetroStudio Plus.lua

--- RetroStudio Plus.lua
+++ RetroStudio Plus.lua
@@ -13,13 +13,13 @@ if game.PlaceId == 5846386835 then
 	CoreGui:WaitForChild("ThemeProvider"):Destroy()
 	CoreGui:WaitForChild("RobloxGui"):Destroy()
 
-	local Logo = Player.PlayerGui:WaitForChild("MenuGui").Topbar.Upper.Logo
+	local Logo = Player.PlayerGui:WaitForChild("MenuGui"):WaitForChild("Topbar"):WaitForChild("Upper"):WaitForChild("Logo")
 	Logo.AnchorPoint = Vector2.new(0,0.5)
 	Logo.Position = UDim2.new(0, 5, 0.5, 0)
 
 	local UpperTabButtons = Player.PlayerGui.MenuGui.Topbar.Upper:WaitForChild("TabButtons")
 	UpperTabButtons.Position = UDim2.new(0, 200, 0, 0)
-	local LowerTabButtons = Player.PlayerGui.MenuGui.Topbar.Lower:WaitForChild("TabButtons")
+	local LowerTabButtons = Player.PlayerGui.MenuGui.Topbar:WaitForChild("Lower"):WaitForChild("TabButtons")
 	LowerTabButtons.Position = UDim2.new(0, 200, 0, 0)
 
 	workspace.CurrentCamera:GetPropertyChangedSignal("ViewportSize"):Connect(function()
