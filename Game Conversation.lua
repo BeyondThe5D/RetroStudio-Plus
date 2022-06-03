bf9ed6a7f67174eeba962b8058c7a31c711357f6
BeyondThe5D <92761598+BeyondThe5D@users.noreply.github.com>
2022-06-03 05:45:14 +0100

    Update Game Conversation.lua

--- Game Conversation.lua
+++ Game Conversation.lua
@@ -142,7 +142,7 @@ local Years = {
 				if buttons.SliderSteps.Value == 256 then
 					buttons.SliderPosition.Value = UserGameSettings.MasterVolume * 256
 					buttons.SliderPosition.Changed:Connect(function(volumelevel)
-						UserGameSettings.MasterVolume = volumelevel / buttons.SliderSteps.Value
+						UserGameSettings.MasterVolume = 1 - (volumelevel / buttons.SliderSteps.Value)
 					end)
 				end
 			end
